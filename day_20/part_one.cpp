0b1f29e1ac03de75d9ad548e5e6198fb189123bd
Henrique <henriquel1997@hotmail.com>
2021-07-27 22:33:01 -0300

    Continuing day 20

--- day_20/part_one.cpp
+++ day_20/part_one.cpp
@@ -1,4 +1,5 @@
 #include "..\common.h"
+#include <xmmintrin.h>
 
 #define TILE_SIZE 10
 
@@ -20,51 +21,94 @@ struct Tile{
 	Tile* prev;
 };
 
-//Maybe use a lookup table if this is too slow
+struct List {
+	uint64 size;
+	Tile* start;
+	Tile* end;
+};
+
+void pushTile(List* list, Tile* tile){
+	list->end->next = tile;
+	tile->prev = list->end;
+	list->end = tile;
+
+	list->size++;
+}
+
+Tile* popTile(List* list){
+	Tile* tile = list->start;
+	list->start = tile->next;
+	list->start->prev = 0;
+	tile->next = 0;
+
+	list->size--;
+	return tile;
+}
+
 Side reverseSide(Side side){
 	Side result = 0;
 	for(uint64 i = 0; i < TILE_SIZE; i++){
-		result |= ((side >> i) & ((Side)1)) << (TILE_SIZE - 1);
+		result |= ((side >> i) & ((Side)1)) << (TILE_SIZE - 1 - i);
 	}
 	return result;
 }
 
-void removeTile(Tile tile){
-	if(tile.next) tile.next->prev = tile.prev;
-	if(tile.prev) tile.prev->next = tile.next;
+Tile getTileFlipped(Tile* tile, int flip){
+	Tile flipped = *tile;
+	if(flip == 1){
+		flipped.left = reverseSide(flipped.left);
+		flipped.right = reverseSide(flipped.right);
+		Side aux = flipped.bottom;
+		flipped.bottom = flipped.top;
+		flipped.top = aux;
+	}else if(flip == 2){
+		flipped.top = reverseSide(flipped.top);
+		flipped.bottom = reverseSide(flipped.bottom);
+		Side aux = flipped.left;
+		flipped.left = flipped.right;
+		flipped.right = aux;
+	}else if(flip == 3){
+		Side aux = reverseSide(flipped.top);
+		flipped.top = reverseSide(flipped.bottom);
+		flipped.bottom = aux;
+		aux = reverseSide(flipped.left);
+		flipped.left = reverseSide(flipped.right);
+		flipped.right = aux;
+	}
+	return flipped;
 }
 
-void printTiles(Tile* tile){
-	for (; tile; tile = tile->next){
-		printf("Tile %llu:\n", tile->id);
-		for (uint64 i = 0; i < TILE_SIZE; i++){
-			bool filled = (tile->top >> i) & ((Side)1);
-			if(filled) printf("#");
-			else printf(".");
+bool searchTiles(List* list, uint64 x, uint64 y, Tile** square, uint64 squareSize){
+	uint64 listSize = list->size;
+	for(uint64 count = 0; count < listSize; count++){
+		Tile* tile = popTile(list);
+		for (int i = 0; i < 4; i++) {
+			Tile flipped = getTileFlipped(tile, i);
+
+			if (
+				(x == 0 || square[x - 1][y].right == flipped.left) &&
+				(y == 0 || square[x][y - 1].bottom == flipped.top)
+			) {
+				square[x][y] = flipped;
+
+				if (x == (squareSize - 1) && y == (squareSize - 1)) {
+					return true;
+				}
+
+				uint64 nextX = (x < (squareSize - 1)) ? (x + 1) : 0;
+				uint64 nextY = (nextX > 0) ? y : (y + 1);
+				if (searchTiles(list, nextX, nextY, square, squareSize)) {
+					return true;
+				}
+			}
 		}
-		printf("\n");
-
-		for (uint64 i = 1; i < TILE_SIZE - 1; i++){
-			bool filled = (tile->left >> i) & ((Side)1);
-			if(filled) printf("#");
-			else printf(".");
-			
-			for(uint64 j = 0; j < (TILE_SIZE - 2); j++) printf(".");
-
-			filled = (tile->right >> i) & ((Side)1);
-			if(filled) printf("#");
-			else printf(".");
-
-			printf("\n");
-		}
-
-		for (uint64 i = 0; i < TILE_SIZE; i++){
-			bool filled = (tile->bottom >> i) & ((Side)1);
-			if(filled) printf("#");
-			else printf(".");
-		}
-		printf("\n\n");
+		pushTile(list, tile);
 	}
+	return false;
+}
+
+inline uint64 squareRoot(uint64 value) {
+	return (uint64)_mm_cvtss_f32(_mm_sqrt_ss(_mm_set_ss((float)value)));
 }
 
 int main(){
@@ -126,10 +170,24 @@ int main(){
 	}
 
 	//Search for a solution
-	Tile** square = (Tile**) malloc(tileBytes);
-	Tile* scratchTiles = (Tile*) malloc(tileBytes);
-	memcpy(scratchTiles, tiles, tileBytes);
+	List list;
+	list.size = numTiles;
+	list.start = tiles;
+	list.end = &tiles[numTiles - 1];
 
-	
+	uint64 squareSize = squareRoot(numTiles);
+	Tile* squareMemory = (Tile*) malloc(tileBytes);
+	Tile** square = (Tile**) malloc(squareSize * sizeof(Tile*));
+	for(uint64 i = 0; i < squareSize; i++){
+		square[i] = &squareMemory[i * squareSize];
+	}
+
+	if(searchTiles(&list, 0, 0, square, squareSize)){
+		uint64 end = squareSize - 1;
+		uint64 result = square[0][0].id * square[0][end].id * square[end][0].id * square[end][end].id;
+		printf("Corners multiplied: %llu\n", result);
+	} else {
+		printf("No combinations found\n");
+	}	
 	return 0;
 }
